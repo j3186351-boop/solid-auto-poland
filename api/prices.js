0c3c699b3d7d8b85d05eb28a1163462fa552320b
j3186351-boop <j3186351@gmail.com>
2026-05-07 22:59:40 +0200

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,25 +1,8 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch('https://api.allorigins.win/raw?url=https://www.autocentrum.pl/paliwa/ceny-paliw/');
-    
-    const html = await response.text();
-
-    function findPrice(label) {
-      const regex = new RegExp(label + '[^0-9]*([0-9],[0-9]{2})');
-      const match = html.match(regex);
-      return match ? match[1] + ' zł' : 'brak';
-    }
-
-    res.status(200).json({
-      pb95: findPrice('PB95'),
-      pb98: findPrice('PB98'),
-      diesel: findPrice('ON'),
-      lpg: findPrice('LPG')
-    });
-
-  } catch (e) {
-    res.status(500).json({
-      error: 'Failed to fetch prices'
-    });
-  }
-}
+module.exports = async (req, res) => {
+  res.status(200).json({
+    pb95: "6.45 zł",
+    pb98: "7.12 zł",
+    on: "6.67 zł",
+    lpg: "3.10 zł"
+  });
+};
