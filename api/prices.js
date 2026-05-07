f10bc0101a3369f846bc4e5f87402d258d412933
j3186351-boop <j3186351@gmail.com>
2026-05-07 23:09:17 +0200

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,24 +1,21 @@
 module.exports = async (req, res) => {
   try {
-    const response = await fetch('https://www.autocentrum.pl/paliwa/ceny-paliw/');
-    const html = await response.text();
-
-    const prices = [...html.matchAll(/([0-9],[0-9]{2})/g)]
-      .map(m => m[1]);
+    const response = await fetch('https://api.npoint.io/6f5e0f7f6d54b0e6f3ab');
+    const data = await response.json();
 
     res.status(200).json({
-      pb95: prices[0] ? prices[0] + ' zł' : '6.45 zł',
-      pb98: prices[1] ? prices[1] + ' zł' : '7.10 zł',
-      on: prices[2] ? prices[2] + ' zł' : '6.60 zł',
-      lpg: prices[3] ? prices[3] + ' zł' : '3.05 zł'
+      pb95: data.pb95,
+      pb98: data.pb98,
+      on: data.on,
+      lpg: data.lpg
     });
 
   } catch (e) {
     res.status(200).json({
       pb95: '6.45 zł',
-      pb98: '7.10 zł',
-      on: '6.60 zł',
-      lpg: '3.05 zł'
+      pb98: '7.15 zł',
+      on: '6.65 zł',
+      lpg: '3.10 zł'
     });
   }
 };
